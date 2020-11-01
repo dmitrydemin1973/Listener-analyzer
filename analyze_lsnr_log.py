c9b33ee01e8a6d04a679f7a1d6119c4034df25c7
Dmitry <dmitrydemin1973@gmail.com>
2020-11-01 20:33:00 +0700

    Add support Python 3

--- analyze_lsnr_log.py
+++ analyze_lsnr_log.py
@@ -4,9 +4,8 @@
 A script to analyze the Oracle listener log
 '''
 
-import re
-import os
 import os.path
+import re
 from datetime import datetime
 from optparse import OptionParser
 
@@ -39,9 +38,9 @@ if lsnr_log_file is None :
 		lsnr_log_file = lsnr_log_file.replace('log.xml','listener.log').replace("alert","trace")
 
 if not os.path.isfile(lsnr_log_file):
-	print
-	print "Listener log should be: " + lsnr_log_file
-	print "But file does not exist"
+	print()
+	print("Listener log should be: " + lsnr_log_file)
+	print("But file does not exist")
 	exit(1)
 
 lsnrf = open(lsnr_log_file,"r")
@@ -51,8 +50,8 @@ line_count=0
 dict_program = {}
 col_width = 25
 
-print
-print "Analyzing log: " + lsnr_log_file + "..."
+print()
+print("Analyzing log: " + lsnr_log_file + "...")
 
 # process the log
 for line in lsnrf:
@@ -71,9 +70,9 @@ for line in lsnrf:
 								except KeyError:
 										dict_program[line_datef,line_prog] = 1
 								line_count +=1
-lsnrf.close
+lsnrf.close()
 
-print "done"
+print('done')
 
 # preparing output
 all_keys = dict_program.keys()
@@ -81,55 +80,54 @@ date_list = sorted(list(set([k[0] for k in all_keys])))
 prog_list = sorted(list(set([k[1] for k in all_keys])))
 
 # print output
-print 
-print "Please choose from the following output options:"
-print "1. csv format"
-print "2. text table format"
+print()
+print("Please choose from the following output options:")
+print("1. csv format")
+print("2. text table format")
 output_format = raw_input()
 
-print
-print "Connection Analysis"
-print "==================="
-print "First date in log:                ", date_list[0].strftime("%Y-%m-%d")
-print "Last date in log:                 ", date_list[-1].strftime("%Y-%m-%d")
-print "Number of connections in the log: ", line_count
-print
-print "Connection distribution per day:"
-print "================================"
-print 
-
+print()
+print("Connection Analysis")
+print("===================")
+print("First date in log:                ", date_list[0].strftime("%Y-%m-%d"))
+print("Last date in log:                 ", date_list[-1].strftime("%Y-%m-%d"))
+print("Number of connections in the log: ", line_count)
+print()
+print("Connection distribution per day:")
+print("================================")
+print()
 if output_format == "1":
-	print "Date",
+	print("Date", end=' ')
 	for p in prog_list:
-		print "," + p,
-	print
+		print("," + p, end=' ')
+	print()
 	for d in date_list:
-		print d.strftime("%Y-%m-%d"),
+		print(d.strftime("%Y-%m-%d"), end=' ')
 		for p in prog_list:
 			if dict_program.get((d, p), None):
-				print "," + str(dict_program[d,p]),
+				print("," + str(dict_program[d, p]), end=' ')
 			else:
-				print ",",
-		print
+				print(",", end=' ')
+		print()
 elif output_format == "2":
 	for i in range(len(prog_list)+1):
-		print "|".ljust(col_width + 2,"="),
-	print "|"
-	print "| Date".ljust(col_width + 2),
+		print("|".ljust(col_width + 2, "="), end=' ')
+	print("|")
+	print("| Date".ljust(col_width + 2), end=' ')
 	for p in prog_list:
-		print "| " + p.ljust(col_width)[:col_width],
-	print "|"
+		print("| " + p.ljust(col_width)[:col_width], end=' ')
+	print("|")
 	for i in range(len(prog_list)+1):
-		print "|=".ljust(col_width + 2,"="),
-	print "|"
+		print("|=".ljust(col_width + 2, "="), end=' ')
+	print("|")
 	for d in date_list:
-		print "| " + d.strftime("%Y-%m-%d").ljust(col_width)[:col_width],
+		print("| " + d.strftime("%Y-%m-%d").ljust(col_width)[:col_width], end=' ')
 		for p in prog_list:
 			if dict_program.get((d, p), None):
 				print "| " + str(dict_program[d,p]).ljust(col_width)[:col_width],
 			else:
-				print "|".ljust(col_width + 2),
-		print "|"
+				print("|".ljust(col_width + 2), end=' ')
+		print("|")
 	for i in range(len(prog_list)+1):
-		print "|".ljust(col_width + 2,"="),
-print "|"
+		print("|".ljust(col_width + 2, "="), end=' ')
+print("|")
