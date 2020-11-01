da672f4fa0f161e596a72c4a0984fddff4a7817a
Dmitry <dmitrydemin1973@gmail.com>
2020-11-01 20:55:51 +0700

    Add support Python 3

--- analyze_lsnr_log.py
+++ analyze_lsnr_log.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-'''
-A script to analyze the Oracle listener log
-'''
+# '''
+# A script to analyze the Oracle listener log
+# '''
 
 import os.path
 import re
@@ -10,43 +10,46 @@ from datetime import datetime
 from optparse import OptionParser
 
 # process command line options
+import pip._vendor.distlib.compat
+
 parser = OptionParser(usage="usage: %%prog\n%s" % __doc__)
 parser.add_option("-f", "--file", dest="filename",
-						default = None,
-						help = "Name of the listener logfile. When specified, we won't attempt to query the running listener process.")
+                  default=None,
+                  help="Name of the listener logfile. When specified, we won't attempt to query the running listener "
+                       "process.")
 
 (options, args) = parser.parse_args()
 lsnr_log_file = options.filename
 
-if lsnr_log_file is None :
-	# get the listener log, if not specified as a command line argument
-	lsnr_stat = os.popen("lsnrctl status").read().rstrip()
-	lsnr_log_line_search = re.search(r'Listener Log File.*$', lsnr_stat, re.M|re.I)
-	if lsnr_log_line_search is None:
-		# can't find 'Listener Log File'
-		print
-		print "Cannot find 'Listener Log File' entry in 'lsnrctl status' output"
-		print "'lsnrctl status' output is:"
-		print
-		print lsnr_stat
-		exit(1)
-	
-	lsnr_log_line = lsnr_log_line_search.group()
-	lsnr_log_file = re.sub(r'Listener Log File *','',lsnr_log_line)
-	if re.search(r'\.xml$', lsnr_log_file, re.M|re.I):
-		# file is xml, replace the "alert" with "trace" and the "log.xml" with "listener.log"
-		lsnr_log_file = lsnr_log_file.replace('log.xml','listener.log').replace("alert","trace")
+if lsnr_log_file is None:
+    # get the listener log, if not specified as a command line argument
+    lsnr_stat = os.popen("lsnrctl status").read().rstrip()
+    lsnr_log_line_search = re.search(r'Listener Log File.*$', lsnr_stat, re.M | re.I)
+    if lsnr_log_line_search is None:
+        # can't find 'Listener Log File'
+        print()
+        print("Cannot find 'Listener Log File' entry in 'lsnrctl status' output")
+        print("'lsnrctl status' output is:")
+        print()
+        print(lsnr_stat)
+        exit(1)
+
+    lsnr_log_line = lsnr_log_line_search.group()
+    lsnr_log_file = re.sub(r'Listener Log File *', '', lsnr_log_line)
+    if re.search(r'\.xml$', lsnr_log_file, re.M | re.I):
+        # file is xml, replace the "alert" with "trace" and the "log.xml" with "listener.log"
+        lsnr_log_file = lsnr_log_file.replace('log.xml', 'listener.log').replace("alert", "trace")
 
 if not os.path.isfile(lsnr_log_file):
-	print()
-	print("Listener log should be: " + lsnr_log_file)
-	print("But file does not exist")
-	exit(1)
+    print()
+    print("Listener log should be: " + lsnr_log_file)
+    print("But file does not exist")
+    exit(1)
 
-lsnrf = open(lsnr_log_file,"r")
+lsnrf = open(lsnr_log_file, "r")
 
 # variables
-line_count=0
+line_count = 0
 dict_program = {}
 col_width = 25
 
@@ -55,21 +58,22 @@ print("Analyzing log: " + lsnr_log_file + "...")
 
 # process the log
 for line in lsnrf:
-		if "* establish *" in line:
-				line_date = line.split("*")[0].split(" ")[0]
-				line_datef = datetime.strptime(line_date,"%d-%b-%Y")
-				line_connectdata = line.split("*")[1]
-				line_address = line.split("*")[1]
-				data_search = re.search(r'PROGRAM=[^\)]*', line_connectdata, re.M|re.I)
-				if data_search is not None:
-						line_prog = data_search.group().split("=")[1].split("\\")[-1]
-                                                if line_prog == "" : line_prog = "*UNSPECIFIED*"
-						if line_prog != "null":
-								try:
-										dict_program[line_datef,line_prog] += 1
-								except KeyError:
-										dict_program[line_datef,line_prog] = 1
-								line_count +=1
+    if "* establish *" in line:
+        line_date = line.split("*")[0].split(" ")[0]
+        line_datef = datetime.strptime(line_date, "%d-%b-%Y")
+        line_connectdata = line.split("*")[1]
+        line_address = line.split("*")[1]
+        data_search = re.search(r"PROGRAM=[^\)]*", line_connectdata, re.M | re.I)
+        if data_search is not None:
+            line_prog = data_search.group().split("=")[1].split("\\")[-1]
+            if line_prog == "":
+                line_prog = "*UNSPECIFIED*"
+            if line_prog != "null":
+                try:
+                    dict_program[line_datef, line_prog] += 1
+                except KeyError:
+                    dict_program[line_datef, line_prog] = 1
+                line_count += 1
 lsnrf.close()
 
 print('done')
@@ -84,7 +88,7 @@ print()
 print("Please choose from the following output options:")
 print("1. csv format")
 print("2. text table format")
-output_format = raw_input()
+output_format = pip._vendor.distlib.compat.raw_input()
 
 print()
 print("Connection Analysis")
@@ -97,37 +101,37 @@ print("Connection distribution per day:")
 print("================================")
 print()
 if output_format == "1":
-	print("Date", end=' ')
-	for p in prog_list:
-		print("," + p, end=' ')
-	print()
-	for d in date_list:
-		print(d.strftime("%Y-%m-%d"), end=' ')
-		for p in prog_list:
-			if dict_program.get((d, p), None):
-				print("," + str(dict_program[d, p]), end=' ')
-			else:
-				print(",", end=' ')
-		print()
+    print("Date", end=' ')
+    for p in prog_list:
+        print("," + p, end=' ')
+    print()
+    for d in date_list:
+        print(d.strftime("%Y-%m-%d"), end=' ')
+        for p in prog_list:
+            if dict_program.get((d, p), None):
+                print("," + str(dict_program[d, p]), end=' ')
+            else:
+                print(",", end=' ')
+        print()
 elif output_format == "2":
-	for i in range(len(prog_list)+1):
-		print("|".ljust(col_width + 2, "="), end=' ')
-	print("|")
-	print("| Date".ljust(col_width + 2), end=' ')
-	for p in prog_list:
-		print("| " + p.ljust(col_width)[:col_width], end=' ')
-	print("|")
-	for i in range(len(prog_list)+1):
-		print("|=".ljust(col_width + 2, "="), end=' ')
-	print("|")
-	for d in date_list:
-		print("| " + d.strftime("%Y-%m-%d").ljust(col_width)[:col_width], end=' ')
-		for p in prog_list:
-			if dict_program.get((d, p), None):
-				print "| " + str(dict_program[d,p]).ljust(col_width)[:col_width],
-			else:
-				print("|".ljust(col_width + 2), end=' ')
-		print("|")
-	for i in range(len(prog_list)+1):
-		print("|".ljust(col_width + 2, "="), end=' ')
+    for i in range(len(prog_list) + 1):
+        print("|".ljust(col_width + 2, "="), end=' ')
+    print("|")
+    print("| Date".ljust(col_width + 2), end=' ')
+    for p in prog_list:
+        print("| " + p.ljust(col_width)[:col_width], end=' ')
+    print("|")
+    for i in range(len(prog_list) + 1):
+        print("|=".ljust(col_width + 2, "="), end=' ')
+    print("|")
+    for d in date_list:
+        print("| " + d.strftime("%Y-%m-%d").ljust(col_width)[:col_width], end=' ')
+        for p in prog_list:
+            if dict_program.get((d, p), None):
+                print("| " + str(dict_program[d, p]).ljust(col_width)[:col_width], end=' ')
+            else:
+                print("|".ljust(col_width + 2), end=' ')
+        print("|")
+    for i in range(len(prog_list) + 1):
+        print("|".ljust(col_width + 2, "="), end=' ')
 print("|")
